37ccd454eea2f64b4ae6ecbe4f64e81c82e07c1e
Alex Van Camp <email@alexvan.camp>
2015-06-01 00:32:13 -0500

    Port from node-ipc to axon, make logging better

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 'use strict';
 
-var config = require('hi-config'); config.load('config.json');
-var os     = require('os');
-var format = require('util').format;
-var ipc    = require('node-ipc');
-var slack  = require('./lib/slack');
-var irc    = require('twitch-irc');
-//var db     = require('twitch-irc-db')({database: './data'});
-//var api    = require('twitch-irc-api');
+var log       = require('./lib/log');
+var config    = require('hi-config'); config.load('config.json');
+var format    = require('util').format;
+var slack     = require('./lib/slack');
+var irc       = require('twitch-irc');
+var rpc       = require('axon-rpc');
+var axon      = require('axon');
+var rep       = axon.socket('rep');
+var pubSock   = axon.socket('pub');
+var rpcServer = new rpc.Server(rep);
 
-ipc.config.id        = 'streen';
-ipc.config.retry     = 1500;
-ipc.config.silent    = true;
-var siphonsByChannel = {};
-var client           = new irc.client({
+var socketsBound      = false;
+var heartbeatTimeouts = {};
+var PUB_PORT          = config.get('pubPort') || 9455;
+var RPC_PORT          = config.get('rpcPort') || 9456;
+var HEARTBEAT_TIMEOUT = 15 * 1000;
+var ircClient         = new irc.client({
     options: {
         exitOnError: false,
         database: './data'
@@ -24,170 +27,166 @@ var client           = new irc.client({
     }
 });
 
+//var db     = require('twitch-irc-db')({database: './data'});
+//var api    = require('twitch-irc-api');
+
 // twitch-irc can be leaky. This enables a sysadmin to pass '--expose-gc' and force a full GC cycle every 15 minutes.
 if (global.gc) {
-    console.log('Running manual garbage collection every 15 minutes');
-    setInterval(function() {
+    log.info('Running manual garbage collection every 15 minutes');
+    setInterval(function () {
         global.gc();
     }, 15 * 60 * 1000);
 }
 
 // Oh no
-process.on('unhandledException', function(err) {
-    console.error(err.stack);
+process.on('unhandledException', function (err) {
+    log.error(err.stack);
     slack.send(format('I\'ve encountered an unhandled error, and will now exit:```%s```', err.stack));
-    ipc.server.broadcast('crash', err);
+    pubSock.send('crash', err);
     process.exit(1);
 });
 
 process.on('SIGINT', function () {
-    console.log('Exiting from SIGINT in one second'.notice);
+    log.info('Exiting from SIGINT in one second');
     slack.send('I\'m exiting from a deliberate SIGINT. This was probably intentional.');
-    setTimeout(function() {
+    setTimeout(function () {
         process.exit(0);
     }, 1000);
 });
 
 // Monkey-patch the broken fastReconnect method.
 // This patch has a drawback of not being zero-downtime, but it's dang close.
-client.fastReconnect = function() {
+ircClient.fastReconnect = function () {
     this.disconnect();
     this.connect();
-}.bind(client);
+}.bind(ircClient);
 
 // Connect to Twitch Chat and listen for various events.
 // Don't start the IPC server until we are connected to Twitch CHat.
-client.connect();
-client
+ircClient.connect();
+ircClient
     .addListener('connected', function () {
-        console.log('Connected to Twitch Chat'.good);
+        log.info('Connected to Twitch Chat');
         slack.send('I\'ve connected to Twitch Chat. So many voices…');
-        if (ipc.server) ipc.server.broadcast('connected');
-        startIPCServer();
+        if (socketsBound) {
+            pubSock.send('connected');
+        } else {
+            bindSockets();
+        }
     })
 
     .addListener('disconnected', function (reason) {
-        console.warn('DISCONNECTED:'.warn, reason);
+        log.warn('DISCONNECTED:', reason);
         slack.send('I\'ve disconnected from Twitch Chat. I will attempt to reconnect for as long as it takes.');
-        ipc.server.broadcast('disconnected');
+        pubSock.send('disconnected');
     })
 
     .addListener('reconnect', function () {
-        console.log('Attempting to reconnect...'.notice);
+        log.info('Attempting to reconnect...');
         slack.send('Attempting to reconnect…');
-        ipc.server.broadcast('reconnect');
+        pubSock.send('reconnect');
     })
 
     .addListener('connectfail', function () {
-        console.error('Failed to connect, reached maximum number of retries'.error);
+        log.error('Failed to connect, reached maximum number of retries');
         slack.send('I\'ve failed to connect to Twitch Chat after reaching the max number of retries!');
-        ipc.server.broadcast('connectfail');
+        pubSock.send('connectfail');
     })
 
     .addListener('limitation', function (err) {
-        console.error('Limitation:'.error, err);
+        log.error('Limitation:', err);
         slack.send('I\'ve encountered a rate limitation! Check my logs.');
-        ipc.server.broadcast('limitation', err);
+        pubSock.send('limitation', err);
     })
 
     .addListener('crash', function (message, stack) {
-        console.error(stack);
+        log.error(stack);
         slack.send(format('I\'ve encountered an unhandled error, and will now exit:```%s```', stack));
-        ipc.server.broadcast('crash', {message: message, stack: stack});
+        pubSock.send('crash', {message: message, stack: stack});
         process.exit(1);
+    })
+
+    .addListener('join', function (channel, username) {
+        var channelNoPound = channel.replace('#', '');
+        log.info('Joined channel:', channelNoPound);
+        pubSock.send('join', channelNoPound, username);
+    })
+
+    .addListener('part', function (channel, username) {
+        var channelNoPound = channel.replace('#', '');
+        log.info('Parted channel:', channelNoPound);
+        pubSock.send('part', channelNoPound, username);
+    })
+
+    .addListener('chat', function (channel, user, message) {
+        var channelNoPound = channel.replace('#', '');
+        pubSock.send('chat', channelNoPound, user, message);
     })
 
     .addListener('subscription', onSubscription)
 
     .addListener('subanniversary', onSubscription)
 
-    .addListener('chat', function (channel, user, message) {
+    .addListener('timeout', function (channel, username) {
         var channelNoPound = channel.replace('#', '');
-        multicastToChannel(channelNoPound, 'chat', {
-            channel: channelNoPound,
-            user: user,
-            message: message
-        });
+        pubSock.send('timeout', channelNoPound, username);
+    })
+
+    .addListener('clearchat', function (channel) {
+        var channelNoPound = channel.replace('#', '');
+        pubSock.send('clearchat', channelNoPound);
     });
 
+function bindSockets() {
+    pubSock.bind(PUB_PORT);
+    rep.bind(RPC_PORT);
+    pubSock.send('connected');
 
-function startIPCServer() {
-    var serveFn = os.platform() === 'win32' ? ipc.serveNet : ipc.serve;
-    serveFn(function () {
-        /*ipc.server.on('apiCall', function (data, socket) {
-         api.call(data.opts, db, function(err, statusCode, response) {
-         if (err) {
-         console.log(err);
-         return;
-         }
-         console.log('Status code: ' + statusCode);
-         console.log('Response from Twitch API:');
-         console.log(JSON.stringify(response));
-         socket.emit('apiResponse', response);
-         });
-         });*/
+    socketsBound = true;
 
-        ipc.server.broadcast('connected');
-
-        // Streen does not join any channels on its own.
-        // It waits for a Siphon to request that it join an array of channels.
-        ipc.server.on('join', function (channels, socket) {
-            channels.forEach(function(channel) {
-                client.join('#' + channel).then(function() {
-                    console.log('Joined channel'.debug, channel.data);
-                    if (!siphonsByChannel.hasOwnProperty(channel)) siphonsByChannel[channel] = new WeakSet();
-                    siphonsByChannel[channel].add(socket);
-                    ipc.server.emit(socket, 'joined', channel);
-                });
+    // Streen does not join any channels on its own.
+    // It waits for a Siphon to request that it join an array of channels.
+    rpcServer.expose('join', function (channel, fn) {
+        if (ircClient.currentChannels.indexOf(channel) >= 0) {
+            // Already in channel, invoke callback with the name
+            fn(null, channel);
+        } else {
+            ircClient.join('#' + channel).then(function () {
+                resetHeartbeat(channel);
             });
-        });
-
-        // When a Siphon (socket) closes, remove it from any channel sets that it is in.
-        ipc.server.on('socket.disconnected', function(socket, destroyedSocketId) {
-            for (var channel in siphonsByChannel) {
-                if (!siphonsByChannel.hasOwnProperty(channel)) continue;
-                siphonsByChannel[channel].delete(socket);
-
-                // If there are no more sockets listening to this channel, then part
-                var hasActiveSocket = false;
-                ipc.server.sockets.forEach(function(socket) {
-                    if (siphonsByChannel[channel].has(socket)) {
-                        hasActiveSocket = true;
-                    }
-                });
-                if (!hasActiveSocket) {
-                    console.log('Parted channel'.debug, channel.data);
-                    client.part('#' + channel);
-                    delete siphonsByChannel[channel];
-                }
-            }
-        });
-
-        // TODO: Make proper RPC with callbacks
-        ipc.server.on('timeout', function (data, socket) {
-            client.timeout(data.channel, data.username, data.seconds).then(function() {
-                ipc.server.emit(socket, 'timedOut', data);
-            });
-        });
-    });
-
-    ipc.server.start();
-}
-
-// Multicasts an event only to sockets that are interested in a given channel
-function multicastToChannel(channel, event, data) {
-    if (!siphonsByChannel.hasOwnProperty(channel)) return;
-    ipc.server.sockets.forEach(function(socket) {
-        if (siphonsByChannel[channel].has(socket)) {
-            ipc.server.emit(socket, event, data);
+            fn(null, null);
         }
     });
+
+    rpcServer.expose('timeout', function (channel, username, seconds, fn) {
+        ircClient.timeout(channel, username, seconds).then(fn);
+    });
+
+    rpcServer.expose('heartbeat', function (channels, fn) {
+        channels.forEach(resetHeartbeat);
+        fn(null, HEARTBEAT_TIMEOUT);
+    });
+}
+
+// Siphons must send a heartbeat every HEARTBEAT_TIMEOUT seconds.
+// Otherwise, their channels are parted.
+// A siphon can miss no more than one consecutive heartbeat.
+function resetHeartbeat(channel, fn) {
+    fn = fn || function () {};
+    clearTimeout(heartbeatTimeouts[channel]);
+    heartbeatTimeouts[channel] = setTimeout(function () {
+        ircClient.part('#' + channel).then(function () {
+            clearTimeout(heartbeatTimeouts[channel]);
+            delete heartbeatTimeouts[channel];
+            fn(null, channel);
+        });
+    }, HEARTBEAT_TIMEOUT * 2 + 1000);
 }
 
 // Formats subscription and subanniversary events before emitting them
 function onSubscription(channel, username, months) {
     var channelNoPound = channel.replace('#', '');
-    multicastToChannel(channelNoPound, 'subscription', {
+    pubSock.send('subscription', {
         channel: channelNoPound,
         username: username,
         resub: !!months,
